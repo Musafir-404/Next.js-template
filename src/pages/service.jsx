c6a74ec10f3a634c818a0f61ab1dbec2e3e2d071
suika <raffidahmad@gmail.com>
2023-04-01 17:07:17 +0500

    fixes

--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -1,16 +1,29 @@
 import { useRouter } from 'next/router';
 import ServicePage from '../components/ServicePage';
 import { services } from '../data/services';
+import Head from 'next/head';
 const Service = () => {
   const router = useRouter();
   const { sid } = router.query;
 
   const service = services.find((service) => service.path === sid);
-console.log('service', sid);
+
   if (!service) {
     return null;
   } else {
-    return <ServicePage {...service} />;
+    return (
+      <>
+        <Head>
+          <title>{service.title}</title>
+          <meta name='description' content={service.desc} />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <main>
+          <ServicePage {...service} />
+        </main>
+      </>
+    );
   }
 };
 
