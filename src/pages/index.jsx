f7cf9772ca932badd9c2e2f117a54b7865f6db80
suika <raffidahmad@gmail.com>
2023-04-01 17:33:57 +0500

    meta desc

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,10 @@ export default function HomePage() {
     <>
       <Head>
         <title>Datafield</title>
-        <meta name='description' content='Datafield' />
+        <meta
+          name='description'
+          content='Datafield, a company that provides data services like tableau, power bi, data management, embedded analytics and staffing augmentation.'
+        />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
