9049acc7e5069d66b8947cd078dbed2de33d7835
suika <raffidahmad@gmail.com>
2023-04-01 17:27:06 +0500

    desc about

--- src/components/SimpleCard/index.jsx
+++ src/components/SimpleCard/index.jsx
@@ -4,8 +4,8 @@ function SimpleCard({ index, title, desc, link }) {
   };
   return (
     <div className='px-16 cursor-pointer hover:text-sky-700 shadow p-4 rounded-lg' onClick={() => handleClick(link)}>
-      <h1 className='text-4xl font-extrabold mb-3.5'>0{index}</h1>
-      <h2 className='text-3xl font-semibold mb-2.5'>{title}</h2>
+      <span className='text-4xl font-extrabold mb-3.5'>0{index}</span>
+      <p className='text-3xl font-semibold mb-2.5'>{title}</p>
       <p className='text-sm text-gray-500 mb-4 '>{desc}</p>
     </div>
   );
