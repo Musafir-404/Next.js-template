47b3c0e6aa9283b2bcd50c5aa1b7c39fbc8a1de8
suika <raffidahmad@gmail.com>
2023-04-01 15:34:21 +0500

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'wave-pattern': "url('/bg-wave.svg')",
+        'wave-pattern-2': "url('/bg-2.svg')"
+      },
+      animation: {
+        'scale-in': 'zoom 1s linear',
+        'fade-in': 'fadeIn 1.5s linear',
+        'in-view-bottom': 'in-view-bottom 1s ease-in-out',
+        'in-view-top': 'in-view-top 1s ease-in-out'
+      },
+      keyframes: {
+        zoom: {
+          '0%': { transform: 'scale(0)' },
+          '100%': { transform: 'scale(1)' }
+        },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' }
+        },
+        'in-view-bottom': {
+          '0%': { transform: 'translateY(20%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' }
+        },
+        'in-view-top': {
+          '0%': { transform: 'translateY(-20%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' }
+        }
+      }
+    }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
